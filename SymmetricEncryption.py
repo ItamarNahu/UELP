860616c3d119dc7c85b0f0ed18bbb58476c6e6a7
ItamarNahu <150347917+ItamarNahu@users.noreply.github.com>
2024-01-15 14:16:56 +0200

    added types to encryption class

--- SymmetricEncryption.py
+++ SymmetricEncryption.py
@@ -6,7 +6,7 @@ from Cryptodome.Util.Padding import pad, unpad
 
 
 class AES_hash_cipher:
-    def __init__(self, key):
+    def __init__(self, key: str):
         """
         builder function to create a new object of AES_hash_cipher with a key
         :param key: key to use for both sides
@@ -14,7 +14,7 @@ class AES_hash_cipher:
         self.bs = AES.block_size
         self.key = hashlib.sha256(key.encode()).digest()
 
-    def encrypt(self, raw):
+    def encrypt(self, raw: bytes) -> bytes:
         """
         method encrypts raw data gotten with AES encryption of key
         :param raw: data to encrypt
@@ -27,7 +27,7 @@ class AES_hash_cipher:
 
         return base64.b64encode(iv + cipher.encrypt(raw))
 
-    def decrypt(self, enc):
+    def decrypt(self, enc: bytes) -> str:
         """
         method decrypts encrypted data gotten with AES decryption of key
         :param enc: encoded encrypted data
@@ -41,7 +41,7 @@ class AES_hash_cipher:
         return unpad(cipher.decrypt(enc[AES.block_size:]), AES.block_size).decode('utf-8')
 
     @staticmethod
-    def hash(data):
+    def hash(data: str) -> bytes:
         """
         method gets data and uses SHA-256 to return it's hash
         :param data: data to use hash function on
