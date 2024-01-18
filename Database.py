962200b38658ba82f4acdffd7d673b1ce19d69e1
ItamarNahu <150347917+ItamarNahu@users.noreply.github.com>
2024-01-18 09:48:36 +0200

    בסיס נתונים גמור

--- Database.py
+++ Database.py
@@ -1,11 +1,13 @@
 import sqlite3
+from SymmetricEncryption import AES_hash_cipher
 
 
 class Database_comm:
     def __init__(self):
         """
-
+        builder function creates a new Database_comm object with table and database names
         """
+
         self.DBname = "Uelp_db.sql"
         self.conn = None
         self.curr = None
@@ -15,14 +17,17 @@ class Database_comm:
 
     def _create_db(self):
         """
-
-        :return:
+        function creates new tables USERS and MACS if thy don't already exist
+        :return: nothing
         """
+
+        # connect to DB
         self.conn = sqlite3.connect(self.DBname)
         self.curr = self.conn.cursor()
+        # create tables
         self.curr.execute(
-            "CREATE TABLE IF NOT EXISTS " + self.loginTableName + "(username VARCHAR(25), password VARCHAR(32), PRIMARY "
-                                                                  "KEY(username))")
+            "CREATE TABLE IF NOT EXISTS " + self.loginTableName + "(username VARCHAR(25), password VARCHAR(32),"
+                                                                  " PRIMARY KEY(username))")
         self.curr.execute(
             "CREATE TABLE IF NOT EXISTS " + self.blacklistTableName + "(mac VARCHAR(32), PRIMARY KEY(mac))")
 
@@ -30,20 +35,65 @@ class Database_comm:
 
     def _checkUser(self, username):
         """
-
-        :param username:
-        :return:
+        function checks if username exists in USERS table
+        :param username: username gotten from user
+        :return: True if username exists in USERS and False otherwise
         """
+
         self.curr.execute("SELECT username FROM " + self.loginTableName + " WHERE username = ?", (username,))
         return self.curr.fetchone() is not None
 
     def addUser(self, username, password):
         """
-
-        :param username:
-        :param password:
-        :return:
+        function adds username and hashed password to USERS table if the username does not already exist
+        :param username: username to add to USERS
+        :param password: password to add to USERS
+        :return: True if successfully added and False otherwise
         """
 
-        if self._checkUser(username):
-            self
+        # check if username already exists in table if it dose not add it and his hashed password into USERS
+        if len(username) < 26 and not self._checkUser(username):
+            self.curr.execute("INSERT INTO " + self.loginTableName + " (username, password) VALUES (?, ?)",
+                              (username, AES_hash_cipher.hash(password)))
+            self.conn.commit()
+            return True
+        return False
+
+    def checkPassword(self, username, password):
+        """
+        function checks if usernames password gotten exists in table
+        :param username: username to check in USERS
+        :param password: password to check in USERS
+        :return: True if password of username gotten exists for username in USERS
+         and False if username or password don't exist in USERS
+        """
+
+        if len(username) < 26 and self._checkUser(username):
+            self.curr.execute("SELECT password FROM " + self.loginTableName + " WHERE username = ?", (username,))
+            password_in_tb = self.curr.fetchone()
+            return password_in_tb == AES_hash_cipher.hash(password)
+        return False
+
+    def macExists(self, mac):
+        """
+        function checks if mac gotten exists in MACS table
+        :param mac: mac to check if it's hash exists in MACS
+        :return: True if exists False otherwise
+        """
+
+        self.curr.execute("SELECT mac FROM " + self.blacklistTableName + " WHERE mac = ?", (AES_hash_cipher.hash(mac),))
+        return self.curr.fetchone() is not None
+
+    def addBlackMac(self, mac):
+        """
+        function checks if mac gotten exists in MACS and if dosen't adds it's hash
+        :param mac: mac to add to MACS table
+        :return: True if mac was added successfully added and False otherwise
+        """
+
+        if not self.macExists(mac):
+            self.curr.execute("INSERT INTO " + self.blacklistTableName + " (mac) VALUES (?)",
+                              (AES_hash_cipher.hash(mac)))
+            self.conn.commit()
+            return True
+        return False
